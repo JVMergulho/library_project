945d3a59af33864f60439a019df0cd08d7384c53
anasosl <asosl+github@cin.ufpe.br>
2025-02-06 22:42:59 -0300

    Populate PESSOA tables

---
+++ populate_tables/populate_people.sql
@@ -0,0 +1,40 @@
+-- Inserindo dados na tabela Pessoa
+INSERT ALL
+    INTO Pessoa (CPF, Nome, DataNascimento, CEP, NumeroEndereco) VALUES ('12345678901', 'Ryei Moraes', TO_DATE('01/01/1990', 'DD/MM/YYYY'), '12345678', '0')
+    INTO Pessoa (CPF, Nome, DataNascimento, CEP, NumeroEndereco) VALUES ('98765432100', 'Leticia Pedrosa', TO_DATE('01/02/1995', 'DD/MM/YYYY'), '12358232', '152')
+    INTO Pessoa (CPF, Nome, DataNascimento, CEP, NumeroEndereco) VALUES ('56789012345', 'João Omena', TO_DATE('01/03/2000', 'DD/MM/YYYY'), '84561259', '516')
+SELECT * FROM dual;
+
+-- Inserindo dados na tabela Logradouro
+INSERT ALL
+    INTO Logradouro (CEP, Rua) VALUES ('12345678', 'Rua dos Bobos')
+    INTO Logradouro (CEP, Rua) VALUES ('12358232', 'Rua dos Pobres')
+    INTO Logradouro (CEP, Rua) VALUES ('84561259', 'Rua dos Ricos')
+SELECT * FROM dual;
+
+-- Inserindo dados na tabela Telefone
+INSERT ALL
+    INTO Telefone (CPF, Fone) VALUES ('12345678901', '81987654321')
+    INTO Telefone (CPF, Fone) VALUES ('98765432100', '81912345678')
+    INTO Telefone (CPF, Fone) VALUES ('56789012345', '81954654132')
+SELECT * FROM dual;
+
+-- Inserindo dados na tabela Leitor
+INSERT ALL
+    INTO Leitor (CPF, TipodeLeitor, Email, Senha) VALUES ('98765432110', 'P', 'sofia.lima@gmail.com', '123456')
+    INTO Leitor (CPF, TipodeLeitor, Email, Senha) VALUES ('56234565444', 'A', 'pedro.monte', '654321')
+    INTO Leitor (CPF, TipodeLeitor, Email, Senha) VALUES ('15468718901', 'I', 'joao.mergulhao', '987654')
+SELECT * FROM dual;
+
+-- Inserindo dados na tabela Secao
+INSERT ALL
+    INTO Secao (Nome) VALUES ('Infantil')
+    INTO Secao (Nome) VALUES ('Literatura Clássica')
+SELECT * FROM dual;
+
+-- Inserindo dados na tabela Funcionario
+INSERT ALL 
+    INTO Funcionario (CPF, Cargo, Supervisor, Email, Senha, secao) VALUES ('12345678901', 'Bibliotecário', '56789012345', 'ryei.moraes@gmail.com', '123456', 1)
+    INTO Funcionario (CPF, Cargo, Supervisor, Email, Senha, secao) VALUES ('98765432100', 'Chefe da Seção', '98765432100', 'leticia.pedrosa@gmail.com', '654321', 2)
+    INTO Funcionario (CPF, Cargo, Supervisor, Email, Senha, secao) VALUES ('56789012345', 'Chefe da Seção', '56789012345', 'joao.omena@gmail.com', '987654', 1)
+SELECT * FROM dual;
