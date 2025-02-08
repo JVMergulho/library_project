b18b831da5cbdf99ea4a50804586b87ebd25ca02
anasosl <asosl+github@cin.ufpe.br>
2025-02-07 22:48:13 -0300

    create a main.sql to run all scripts

---
+++ main.sql
@@ -0,0 +1,9 @@
+-- criação de tabelas
+START create_tables/create_pessoa.sql
+START create_tables/create_livro.sql
+START create_tables/create_actions.sql
+
+-- populando tabelas
+START populate_tables/populate_people.sql
+START populate_tables/populate_books.sql
+START populate_tables/populate_actions.sql
